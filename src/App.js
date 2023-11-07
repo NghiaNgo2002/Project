f37ca07f5df6cc923938078295d6351e954a9840
NghiaNgo2002 <20521653@gm.uit.edu.vn>
2023-11-07 16:21:15 +0700

    123

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Slider from './components/Slider';
 import NewIn from './components/NewIn';
 import Stack from 'react-bootstrap/Stack';
 import Footer from './components/Footer';
+import Banners from './components/Banners';
+import Features from './components/Features';
 
 
 function App() {
   return (
-    <Stack gap={4} className ="stack">
+    <Stack gap={6} className ="stack">
       <div className="p-2 header"><Header /></div>
       <div className="p-2 slider"><Slider /></div>
       <div className="p-2 newin"><NewIn /></div>
+      <div className="p-2 banners"><Banners /></div>
+      <div className="p-2 features"><Features /></div>
       <footer ><Footer /></footer>
+      
     </Stack>
   );
 }
--- src/components/NewIn.js
+++ src/components/NewIn.js
@@ -13,10 +13,10 @@ function NewIn(){
                 </div>
                 </div>
                 <div className ="row">
-                    <div className ="col-12 col-md-6 col-lg-3">
+                    <div className ="col-12 col-md-6 col-lg-3 meo">
                     <a href="/collections/all" className="shoppable-new-in__category">
                     <img alt="Image" data-sizes="auto" data-srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-1_2000x.jpg?v=1656931670 400w,
-                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-1_2000x.jpg?v=1656931670 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="lazyautosizes lazyloaded" sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-1_2000x.jpg?v=1656931670 400w,
+                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-1_2000x.jpg?v=1656931670 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" className="lazyautosizes lazyloaded hover-scale"  sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-1_2000x.jpg?v=1656931670 400w,
                 //durotan-fashion.myshopify.com/cdn/shop/files/new-in-1_2000x.jpg?v=1656931670 800w"/>
                 <p>Clothings</p>
                 </a>
@@ -24,7 +24,7 @@ function NewIn(){
                 <div className ="col-12 col-md-6 col-lg-3">
                     <a href="/collections/all" className="shoppable-new-in__category">
                     <img alt="Image" data-sizes="auto" data-srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-2_2000x.jpg?v=1656931684 400w,
-                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-2_2000x.jpg?v=1656931684 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="lazyautosizes lazyloaded" sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-2_2000x.jpg?v=1656931684 400w,
+                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-2_2000x.jpg?v=1656931684 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" className="lazyautosizes lazyloaded hover-scale" sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-2_2000x.jpg?v=1656931684 400w,
                 //durotan-fashion.myshopify.com/cdn/shop/files/new-in-2_2000x.jpg?v=1656931684 800w"/>
                 <p>Shoes</p>
                 </a>
@@ -32,7 +32,7 @@ function NewIn(){
              <div className ="col-12 col-md-6 col-lg-3">
                     <a href="/collections/all" className="shoppable-new-in__category">
                     <img alt="Image" data-sizes="auto" data-srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-3_2000x.jpg?v=1656931699 400w,
-                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-3_2000x.jpg?v=1656931699 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="lazyautosizes lazyloaded" sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-3_2000x.jpg?v=1656931699 400w,
+                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-3_2000x.jpg?v=1656931699 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" className="lazyautosizes lazyloaded hover-scale" sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-3_2000x.jpg?v=1656931699 400w,
                 //durotan-fashion.myshopify.com/cdn/shop/files/new-in-3_2000x.jpg?v=1656931699 800w"/>
                 <p>Bags</p>
                 </a>
@@ -40,7 +40,7 @@ function NewIn(){
                 <div className ="col-12 col-md-6 col-lg-3">
                     <a href="/collections/all" className="shoppable-new-in__category">
                     <img alt="Image" data-sizes="auto" data-srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-4_2000x.jpg?v=1656931712 400w,
-                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-4_2000x.jpg?v=1656931712 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="lazyautosizes lazyloaded" sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-4_2000x.jpg?v=1656931712 400w,
+                //durotan-fashion.myshopify.com/cdn/shop/files/new-in-4_2000x.jpg?v=1656931712 800w" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" className="lazyautosizes lazyloaded hover-scale" sizes="290px" srcset="//durotan-fashion.myshopify.com/cdn/shop/files/new-in-4_2000x.jpg?v=1656931712 400w,
                 //durotan-fashion.myshopify.com/cdn/shop/files/new-in-4_2000x.jpg?v=1656931712 800w"/>
                 <p>Accessories</p>
                     </a>
