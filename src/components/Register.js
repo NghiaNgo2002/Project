4470bcd1dff5fe9b097b9a9dae62ecfe8df92dfd
NghiaNgo2002 <nghiango229@gmail.com>
2023-11-09 22:34:05 +0700

    123

--- src/components/Register.js
+++ src/components/Register.js
@@ -9,7 +9,6 @@ function Register() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-
   async function handleSignUp(event) {
     event.preventDefault();
 
@@ -19,15 +18,16 @@ function Register() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({email ,password}),
+        body: JSON.stringify({ email, password }),
       });
 
       if (response.ok) {
+        // Handle successful registration, e.g., redirect to login page
+        window.location.href = '/';
       } else {
         // Handle sign-up failure, show an error message
         const errorData = await response.json();
         setError(errorData.message);
-        console.error('Sign Up failed');
       }
     } catch (error) {
       // Handle network errors, request failures, etc.
@@ -43,64 +43,59 @@ function Register() {
       </div>
       <hr className="custom-hr" />
       <div className="login">
-        <form onSubmit={handleSignUp}>
-          <div className="login-container1">
-            <div className="login__container2">
-              <h1 className="login__title">Register</h1>
-              {error && <div className="error">{error}</div>}
-              <form method="post" action="/account" id="create_customer" acceptCharset="UTF-8" data-login-with-shop-sign-up="true">
-                <input type="hidden" name="form_type" value="create_customer" />
-                <input type="hidden" name="utf8" value="✓" />
-                <div className="login__inputs">
-                  <div className="form-group">
-                    <input type="text" name="customer[first_name]" placeholder="First name" id="input-email" />
-                  </div>
-                  <div className="form-group">
-                    <input type="text" name="customer[last_name]" placeholder="Last name" id="input-lastname" />
-                  </div>
-                  <div className="form-group required">
-                      <input
-                      type="email"
-                      required="required"
-                      name="customer[email]"
-                      placeholder="Email address"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                       />
-                  </div>
-                  <div className="form-group required">
-                      <input
-                      type="password"
-                      required="required"
-                      name="customer[password]"
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                       />
-                  </div>
+        <div className="login-container1">
+          <div className="login__container2">
+            <h1 className="login__title">Register</h1>
+            {error && <div className="error">{error}</div>}
+            <form onSubmit={handleSignUp}>
+              <div className="login__inputs">
+                <div className="form-group">
+                  <input type="text" name="customer[first_name]" placeholder="First name" id="input-email" />
                 </div>
-                <div className="login__button">
-                  <input className="button" type="submit" value="Register" />
+                <div className="form-group">
+                  <input type="text" name="customer[last_name]" placeholder="Last name" id="input-lastname" />
                 </div>
-                <div className="login__forgot-password">
-                  By creating your account, you agree to our<br />
-                  <a href="/pages/terms">Terms & conditions</a> and <a href="/pages/privacy">Privacy policy</a>
+                <div className="form-group required">
+                  <input
+                    type="email"
+                    required="required"
+                    name="customer[email]"
+                    placeholder="Email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
-                <div className="login__register">
-                  <Link to="/login">
-                    <a>I already have an account</a>
-                  </Link>
+                <div className="form-group required">
+                  <input
+                    type="password"
+                    required="required"
+                    name="customer[password]"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
-              </form>
+              </div>
+              <div className="login__button">
+                <button type="submit" className="button">Register</button>
+              </div>
+            </form>
+            <div className="login__forgot-password">
+              By creating your account, you agree to our<br />
+              <a href="/pages/terms">Terms & conditions</a> and <a href="/pages/privacy">Privacy policy</a>
+            </div>
+            <div className="login__register">
+              <Link to="/">
+                <a>I already have an account</a>
+              </Link>
             </div>
           </div>
-        </form>
+        </div>
       </div>
       <div className="p-2 footer">
         <Footer />
       </div>
     </div>
-    
   );
 }
 
